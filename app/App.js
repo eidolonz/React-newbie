aa2405ce17b9961bccceba4490d43b915eaa7097
Unknown <piyawat.chutimavutikul@gmail.com>
2018-05-23 10:34:33 +0700

    Add login with facebook

--- app/App.js
+++ app/App.js
@@ -1,21 +1,59 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import Title from './Component/Title'
-import './App.scss';
+import React, { Component } from 'react'
+import { withCookies, Cookies } from 'react-cookie'
+import FacebookLogin from 'react-facebook-login'
+import logo from './logo.svg'
+import { Title } from './components'
+import ChoicesContainer from './containers/ChoicesContainer'
+import './App.scss'
 
 class App extends Component {
+
+  state = {
+    user: {}
+  }
+
+  componentDidMount() {
+  }
+
+  responseFacebook(response) {
+    const { cookies } = this.props
+    cookies.set('userInfo', response)
+  }
+
+  showUser() {
+    const { cookies } = this.props
+    const user = cookies.get('userInfo')
+    this.setState({
+      user
+    })
+  }
+
   render() {
+    const { user } = this.state
     return (
       <div className="App">
+        <FacebookLogin
+          appId="173270656655251"
+          autoLoad={true}
+          fields="name,email,picture"
+          callback={(res) => {
+            this.responseFacebook(res)
+          }}
+        />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <Title text={'Hello CPu_PiPe'} />
-        
+        <img src={user.picture ? user.picture.data.url : ''} />
+        <div>
+          name : {user.name}
+        </div>
+        <button onClick={() => {
+          this.showUser()
+        }}>user facebook</button>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default withCookies(App)
--- app/index.js
+++ app/index.js
@@ -1,14 +1,22 @@
 import React from 'react'
 import { render } from 'react-dom'
-import './index.scss';
-import App from './App';
+import { Provider } from 'react-redux'
+import { CookiesProvider } from 'react-cookie'
+import App from './App'
+import configureStore from './store/configureStore'
+import './index.scss'
+
 // import registerServiceWorker from './registerServiceWorker';
 const rootEl = document.getElementById('app')
 
 const renderApp = (Component) => {
   console.log('KKKKKKKKKKKKKKKKKKKKKKK', Component, document.getElementById('app'));
   render(
-    <Component />
+    <Provider store={store}>
+      <CookiesProvider>
+        <Component />
+      </CookiesProvider>
+    </Provider >
     ,
     rootEl
   )
